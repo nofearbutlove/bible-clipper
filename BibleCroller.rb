5499f9e64c799832cc1ec37a23931dae77bf0ac2
John Nam <jmyl@me.com>
2017-08-01 07:19:39 +0900

    web과 file read 함수를 분리. BibleCroller debugging 완료!

--- BibleCroller.rb
+++ BibleCroller.rb
@@ -4,7 +4,8 @@ require 'json'
 require_relative "BibleReader.rb"
 
 reader = BibleReader.new
-translation_names = BibleInfo.translation_name_to_code.keys
+
+translation_names = ['개역개정', '새번역', 'NIV']#BibleInfo.translation_name_to_code.keys
 translation_names.each do |translation_name|
   translation_code = BibleInfo.translation_name_to_code[translation_name]
   BibleInfo.bible_name_to_chapter_len.each do |bible_name, chapter_len|
@@ -14,11 +15,12 @@ translation_names.each do |translation_name|
     unless Dir.exist? db_name
       FileUtils.mkdir_p(db_name)
     end
-    
-    (1..chapter_len).each do |chapter|    
+
+    (1..chapter_len).each do |chapter|
       file = File.open "#{db_name}/#{chapter}", 'w'
-      chapter_content = reader.get_chapter(translation_name, bible_shortname, chapter)
+      chapter_content = reader.get_chapter_from_web(translation_name, bible_shortname, chapter)
       file.write(chapter_content.to_json)
+      file.close()
     end
   end
 end
--- BibleReader.rb
+++ BibleReader.rb
@@ -1,7 +1,7 @@
 
 class BibleReader
-  
-  require 'rubygems'    
+
+  require 'rubygems'
   require 'nokogiri'
   require 'open-uri'
   require "fileutils"
@@ -13,88 +13,54 @@ class BibleReader
 
   end
 
-  def get_chapter(translation_name, bible_shortname, chapter)
-    translation_code = BibleInfo.translation_name_to_code[translation_name]
-    bible_name = BibleInfo.bible_shortname_to_name[bible_shortname]
-    bible_code = BibleInfo.bible_name_to_code[bible_name]
-    bible_english_code = BibleInfo.bible_name_to_english_code[bible_name]
-    
-    dir = File.dirname(__FILE__)
-    db_name = "#{dir}/db/#{translation_name}/#{"%02d_" % bible_code}#{bible_name}"
-    # puts db_name
-    file_name = "#{db_name}/#{chapter}"
-    
-    # url ="https://www.biblegateway.com/passage/?search=#{bible_english_code}#{chapter}&version=#{translation_code}"
-    # # puts open(url).read.gsub(/<BR><SPAN>/, '<BR></SPAN><SPAN>')
-    # page = Nokogiri::HTML(open(url).read, nil)
-    # # puts page
-    # # puts page.css('#tdBible1 span').map { |node| node.text.strip }
-    # page.search('.//div').remove
-    # page.search('.//a[@class="comment"]').remove
-    # page.search('.//font[@class="number"]').remove
-    # page.search('.//font[@class="smallTitle"]').remove
-    
-    
-    
-    
-    # puts page.xpath('//td[@id="tdBible1"]')
-    
-    # puts page.css('#tdBible1 span').map { |node| node.text.strip }
-    # page.xpath('//td[@id="tdBible1"]/span').each do |a|
-    #   puts a.content
-    # end
-    
-    # page.css('table li').map { |node| node.text.strip }
-    # page.css('#tdBible1 span').map { |node| node.text.strip }
-    
-    unless File.exist? file_name
-      unless Dir.exist? db_name
-        FileUtils.mkdir_p(db_name)
-      end
+    def get_chapter_from_web(translation_name, bible_shortname, chapter)
+        translation_code = BibleInfo.translation_name_to_code[translation_name]
+        bible_name = BibleInfo.bible_shortname_to_name[bible_shortname]
+        bible_code = BibleInfo.bible_name_to_code[bible_name]
+        bible_english_code = BibleInfo.bible_name_to_english_code[bible_name]
 
-      hangul_translations = ['개역개정', '개역한글', '표준새번역', '새번역', '공동번역', '공동번역 개정판']
-      if hangul_translations.include?(translation_name)#FALSE
-        # puts "한글"
-        # 대한성서공회
-        url = "http://www.bskorea.or.kr/infobank/korSearch/korbibReadpage.aspx?version=#{translation_code}&book=#{bible_english_code}&chap=#{chapter}"
-        # puts open(url).read.gsub(/<BR><SPAN>/, '<BR></SPAN><SPAN>')
-        page = Nokogiri::HTML(open(url).read.gsub(/<BR><SPAN>/, '<BR></SPAN><SPAN>'), nil)
-        # puts page
-        # puts page.css('#tdBible1 span').map { |node| node.text.strip }
-        page.search('.//div').remove
-        page.search('.//a[@class="comment"]').remove
-        page.search('.//font[@class="number"]').remove
-        page.search('.//font[@class="smallTitle"]').remove
-        
-        # puts page.xpath('//td[@id="tdBible1"]')
-        
-        # puts page.css('#tdBible1 span').map { |node| node.text.strip }
-        # page.xpath('//td[@id="tdBible1"]/span').each do |a|
-        #   puts a.content
-        # end
-        
-        # page.css('table li').map { |node| node.text.strip }
-        chapter_content = page.css('#tdBible1 span').map { |node| node.text.strip }
-      else
-        # puts "영어"
-        # Holybible
-        # 개역개정 여호수아 22장 10절 문제 있음.
-        
-        # Bible Gateway
-        # https://www.biblegateway.com/passage/?search=gen1&version=ESV
-        # puts "hi"
-        # puts "https://www.biblegateway.com/passage/?search=#{bible_english_code}#{chapter}&version=#{translation_code}"
-        url = "http://www.holybible.or.kr/B_#{translation_code}/cgi/bibleftxt.php?VR=#{translation_code}&VL=#{bible_code}&CN=#{chapter}&CV=99"
+        hangul_translations = ['개역개정', '개역한글', '표준새번역', '새번역', '공동번역', '공동번역 개정판']
+        if hangul_translations.include?(translation_name)#FALSE
+            url = "http://bible.godpia.com/read/reading.asp?ver=#{translation_code}&ver2=&vol=#{bible_english_code}&chap=#{chapter}&sec="
+            page = Nokogiri::HTML(open(url).read, nil)
+            page.search('.//span[@class="num"]').remove
+            chapter_content = page.css('.wide p').map { |node| node.text.strip }
+        else
+            # puts "영어"
+            # Holybible
+            # 개역개정 여호수아 22장 10절 문제 있음.
 
-        # Holybible!
-        page = Nokogiri::HTML(open(url).read, nil, 'eucKR')
-        chapter_content = page.css('table li').map { |node| node.text.strip }
-      end
-      File.open(file_name, 'w').write(chapter_content.to_json)
-    else
-      buffer = File.open(file_name, 'r').read
-      chapter_content = JSON.parse(buffer)
+            # Bible Gateway
+            # https://www.biblegateway.com/passage/?search=gen1&version=ESV
+            # puts "hi"
+            # puts "https://www.biblegateway.com/passage/?search=#{bible_english_code}#{chapter}&version=#{translation_code}"
+            url = "http://www.holybible.or.kr/B_#{translation_code}/cgi/bibleftxt.php?VR=#{translation_code}&VL=#{bible_code}&CN=#{chapter}&CV=99"
+
+            # Holybible!
+            page = Nokogiri::HTML(open(url).read, nil, 'eucKR')
+            chapter_content = page.css('table li').map { |node| node.text.strip }
+        end
+        if chapter_content.length == 0
+            puts "Website is deactivated!"
+            exit
+        end
+
+        chapter_content
     end
-    chapter_content
-  end
+
+    def get_chapter(translation_name, bible_shortname, chapter)
+        translation_code = BibleInfo.translation_name_to_code[translation_name]
+        bible_name = BibleInfo.bible_shortname_to_name[bible_shortname]
+        bible_code = BibleInfo.bible_name_to_code[bible_name]
+        bible_english_code = BibleInfo.bible_name_to_english_code[bible_name]
+
+        dir = File.dirname(__FILE__)
+        db_name = "#{dir}/db/#{translation_name}/#{"%02d_" % bible_code}#{bible_name}"
+        # puts db_name
+        file_name = "#{db_name}/#{chapter}"
+
+        buffer = File.open(file_name, 'r').read
+        chapter_content = JSON.parse(buffer)
+        chapter_content
+    end
 end
